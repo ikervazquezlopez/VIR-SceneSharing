5b3eed626b07a786b1343f1dde782d13c86bd66f
Ujjwal Acharya <uzwal47@gmail.com>
2016-04-01 10:16:18 -0600

    The client receives data, parses it and it is used in a function to print it. It remains to perform the look_at action.

--- Client/Client/client.cpp
+++ Client/Client/client.cpp
@@ -4,6 +4,7 @@ Silver Moon ( m00n.silv3r@gmail.com )
 */
 
 #include<stdio.h>
+#include<iostream>
 #include<winsock2.h>
 #include<ws2ipdef.h>
 
@@ -12,16 +13,13 @@ Silver Moon ( m00n.silv3r@gmail.com )
 #define BUFLEN 512  //Max length of buffer
 #define PORT 8888   //The port on which to listen for incoming data
 
-int main()
-{
-	SOCKET s;
-	struct sockaddr_in server, si_other;
-	int slen, recv_len;
-	char buf[BUFLEN];
-	WSADATA wsa;
+SOCKET s;
+struct sockaddr_in server, si_other;
+int slen, recv_len;
+char buf[BUFLEN];
+WSADATA wsa;
 
-	slen = sizeof(si_other);
-
+int initialize() {
 	//Initialise winsock
 	printf("\nInitialising Winsock...");
 	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
@@ -29,7 +27,7 @@ int main()
 		printf("Failed. Error Code : %d", WSAGetLastError());
 		exit(EXIT_FAILURE);
 	}
-	printf("Initialised.\n");
+	printf("WSA initialised.\n");
 
 	//Create a socket
 	if ((s = socket(AF_INET, SOCK_DGRAM, 0)) == INVALID_SOCKET)
@@ -49,7 +47,7 @@ int main()
 		printf("Bind failed with error code : %d", WSAGetLastError());
 		exit(EXIT_FAILURE);
 	}
-	puts("Bind done");
+	printf("Bind done.\n");
 
 	struct ip_mreq mreq;
 	mreq.imr_interface.s_addr = htonl(INADDR_ANY);
@@ -57,32 +55,35 @@ int main()
 	setsockopt(s, IPPROTO_IP, 12, (const char *)&mreq, sizeof(mreq)); // 12 = IP_ADD_MEMBERSHIP
 	int err = WSAGetLastError();
 
-	//keep listening for data
+	return 0;
+}
+
+int look_at(int x, int y) {
+	std::cout << "(" << x << "," << y << ")" << std::endl;
+	return 0;
+}
+
+int main()
+{
+	initialize();
+
+	slen = sizeof(si_other);
 	while (1)
 	{
-		printf("Waiting for data...");
-		fflush(stdout);
-
-		//clear the buffer by filling null, it might have previously received data
+		// Clear the buffer
 		memset(buf, '\0', BUFLEN);
 
-		//try to receive some data, this is a blocking call
+		// Receive data
 		if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen)) == SOCKET_ERROR)
 		{
-			printf("recvfrom() failed with error code : %d", WSAGetLastError());
+			std::cout << "recvfrom() failed with error code : " << WSAGetLastError() << std::endl;
 			exit(EXIT_FAILURE);
 		}
 
-		//print details of the client/peer and the data received
-		printf("Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
-		printf("Data: %s\n", buf);
+		int x = atoi(strtok(buf, ";"));
+		int y = atoi(strtok(NULL, ";"));
 
-		//now reply the client with the same data
-		if (sendto(s, buf, recv_len, 0, (struct sockaddr*) &si_other, slen) == SOCKET_ERROR)
-		{
-			printf("sendto() failed with error code : %d", WSAGetLastError());
-			exit(EXIT_FAILURE);
-		}
+		look_at(x, y);
 	}
 
 	closesocket(s);
